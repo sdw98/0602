3095c9e67e1322e7680ef9dbf3fdec07ca6e814f
sdw98 <sdw9851@gmail.com>
2025-06-02 10:34:56 +0900

    산술 연산자

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,23 @@ public class Main {
         int a = 10;
         int b = 3;
 
+        int sum = a + b;
+        System.out.println(sum);
+
+        int difference = a - b;
+        System.out.println(difference);
+
+        int product = a * b;
+        System.out.println(product);
+
+        int quotient = a / b;
+        System.out.println(quotient);
+
+        int remainder = a % b;
+        System.out.println(remainder);
+
+
+
+
     }
 }
