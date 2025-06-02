3681a1d230c19bbc93c983d1d7ad6b0be84597a7
sdw98 <sdw9851@gmail.com>
2025-06-02 15:08:04 +0900

    switch문에서 if문으로 변경

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,28 @@ public class Main {
         // "happy", "angry", "sad", etc.
         String mood = "angry";
 
-        switch (mood) {
-            case "happy":           //mood == "happy" 생략
-                System.out.println("나는 행복하다");
+//        switch (mood) {
+//            case "happy":           //mood == "happy" 생략
+//                System.out.println("나는 행복하다");
+//
+//            case "tired":
+//                System.out.println("나는 피곤하다");
+//
+//            case "sad":
+//                System.out.println("나는 슬프다");
+//                break;
+//
+//            default:
+//                System.out.println("표현불가");
 
-            case "tired":
-                System.out.println("나는 피곤하다");
-
-            case "sad":
-                System.out.println("나는 슬프다");
-                break;
-
-            default:
-                System.out.println("표현불가");
+        if (mood == "happy") {
+            System.out.println("나는행복하다.");
+        } else if(mood == "tired") {
+            System.out.println("나는 피곤하다.");
+        } else if (mood == "sad"){
+            System.out.println("나는 슬프다.");
+        } else {
+            System.out.println("표현불가");
         }
     }
 }
